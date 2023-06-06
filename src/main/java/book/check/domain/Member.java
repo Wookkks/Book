31f17d673719a9fb4d18e23cc891c697f5b61e16
tbehippie <tbehippie00@gmail.com>
2023-06-06 12:17:12 +0900

    domain(Member, Noti) 수정했습니다.

--- src/main/java/book/check/domain/Member.java
+++ src/main/java/book/check/domain/Member.java
@@ -1,5 +1,18 @@
 package book.check.domain;
 
-public class Member {
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter @Setter @NoArgsConstructor
+public class Member {
+	private String m_name;
+	private String m_phone;
+	private String m_address;
+	
+	public Member(String m_name, String m_phone, String m_address) {
+		this.m_name = m_name;
+		this.m_phone = m_phone;
+		this.m_address = m_address;
+	}
 }
--- src/main/java/book/check/domain/Noti.java
+++ src/main/java/book/check/domain/Noti.java
@@ -1,5 +1,20 @@
 package book.check.domain;
 
-public class Noti {
+import java.sql.Date;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter @Setter @NoArgsConstructor
+public class Noti {
+	private String n_title;
+	private String n_content;
+	private Date n_date;
+	
+	public Noti(String n_title, String n_content, Date n_date) {
+		this.n_title = n_title;
+		this.n_content = n_content;
+		this.n_date = n_date;
+	}
+	
 }
