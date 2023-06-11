b9de0b21acbb33f714c2a7252cd3ded8eae98108
Wookkks <discdao@gmail.com>
2023-06-11 15:42:23 +0900

    u_how, h_join html  파일 css 링크 경로 수정 전 <link rel="stylesheet" href="css/u_how.css" /> 후 <link rel="stylesheet" href="/css/u_how.css" />
    Service
    Review, WithBook 추가

--- src/main/java/book/check/service/WithBookService.java
+++ src/main/java/book/check/service/WithBookService.java
@@ -1,5 +1,37 @@
 package book.check.service;
 
-public class WithBookService {
+import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import book.check.domain.WithBook;
+import book.check.repository.WithBookRepository;
+
+@Service
+public class WithBookService {
+	
+	@Autowired
+	WithBookRepository withBookRepository;
+	
+	public WithBook saveWithBook(WithBook withBook) {
+		return withBookRepository.saveWithBook(withBook);
+	}
+	
+	public Optional<WithBook> findByNo(Long w_no){
+		return withBookRepository.findByNo(w_no);
+	}
+	
+	public List<WithBook> findAll(){
+		return withBookRepository.findAll();
+	}
+	
+	public List<WithBook> search(String title){
+		return withBookRepository.search(title);
+	}
+	
+	public WithBook updateWithBook(Long w_no, WithBook withBook) {
+		return withBookRepository.updateWithBook(w_no, withBook);
+	}
 }
