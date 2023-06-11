9cb2575c79e3a4a13633702cfb72b9672c3b1b5f
tbehippie <tbehippie00@gmail.com>
2023-06-12 00:51:37 +0900

    Service 초안 작성(MemberService, NotiService)

--- src/main/java/book/check/service/MemberService.java
+++ src/main/java/book/check/service/MemberService.java
@@ -1,5 +1,28 @@
 package book.check.service;
 
-public class MemberService {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import book.check.domain.Member;
+import book.check.repository.MemberRepository;
+
+@Service
+public class MemberService {
+	
+	//1. 생성자 주입방식
+	@Autowired
+	private final MemberRepository memberRepository;
+	
+	public MemberService(MemberRepository memberRepository) {
+		this.memberRepository = memberRepository;
+	}
+	
+	//2. 필드주입 방식
+	// @Autowired
+	// MemberRepository memberRepository;
+
+	//가입신청
+	public Member saveMember(Member member) {
+		return memberRepository.saveMember(member);
+	}
 }
--- src/main/java/book/check/service/NotiService.java
+++ src/main/java/book/check/service/NotiService.java
@@ -1,5 +1,55 @@
 package book.check.service;
 
-public class NotiService {
+import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import book.check.domain.Noti;
+import book.check.repository.NotiRepository;
+
+@Service
+public class NotiService {
+	// 1. 생성자 주입
+	// @Autowired
+	// interface NotiRepository
+	//	private final NotiRepository notiRepository;
+
+	//	public NotiService(NotiRepository notiRepository) {
+	//		this.notiRepository = notiRepository;
+
+	// 2. 필드주입
+	@Autowired
+	NotiRepository notiRepository;
+
+	// 공지등록
+	public void saveNoti(Noti noti) {
+		notiRepository.saveNoti(noti);
+	}
+
+	// 공지 등록번호로 조회
+	Optional<Noti> findByNo(Long n_no) {
+		return notiRepository.findByNo(n_no);
+	}
+
+	// 공지 제목으로 조회
+	Optional<Noti> findByTitle(String n_title) {
+		return notiRepository.findByTitle(n_title);
+	}
+
+	// 공지 전체목록
+	List<Noti> findAll() {
+		return notiRepository.findAll();
+	}
+
+	// 공지수정
+	Noti updateNoti(Long n_no, Noti update_noti) {
+		return notiRepository.updateNoti(n_no, update_noti);
+	}
+
+	// 공지삭제
+	void deleteNoti(Long n_no) {
+		notiRepository.deleteNoti(n_no);
+	}
 }
