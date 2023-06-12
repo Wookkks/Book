8430d4510bb032c6389e5f19a86bf82893094c60
tbehippie <tbehippie00@gmail.com>
2023-06-13 00:11:30 +0900

    NotiController 관리자부분에서 NotiService주입하는 코드(현재 주석처리), user/join페이지 '뒤로가기' img태그에 history.back()추가

--- src/main/java/book/check/controller/ManagerController.java
+++ src/main/java/book/check/controller/ManagerController.java
@@ -1,5 +1,6 @@
 package book.check.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,20 +10,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import book.check.domain.Noti;
+import book.check.service.NotiService;
 import lombok.extern.slf4j.Slf4j;
 
 @Controller
 @Slf4j
 @RequestMapping("/manager")
 public class ManagerController {
-	//<관리자>
 	
-//	작성 시작 부//Service단 생성예정. Repository랑 둘 다 의존성 주입 예정.  
-//	private final NotiRepository notiRepository;
-//	
+//	private final NotiService notiService;
+//
 //	@Autowired
-//	public NotiController(NotiRepository notiRepository) {
-//		this.notiRepository = notiRepository;
+//	public NotiController(NotiService notiService) {
+//		this.notiService = notiService;
 //	}
 	
 	//공지사항
@@ -31,7 +31,8 @@ public class ManagerController {
 		return "manager/m_noti";
 	}
 		
-	//공지사항 상세 X //공지사항 페이지에 목록들 전체 뜨고 각각 수정/삭제버튼 있음
+	//공지사항 상세 X
+	//공지사항 페이지에 목록들 전체 뜨고 각각 수정/삭제버튼 있음
 	
 	//공지사항 등록 폼
 	@GetMapping("add/noti")
