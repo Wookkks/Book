8d711d475389384c287e4c73cc899f50f8134c9f
wong1121 <125429551+wong1121@users.noreply.github.com>
2023-06-14 08:49:49 +0900

    프론트 main.html, main,js

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,24 +1,39 @@
-// modal
-const modal = document.querySelector('.modal');
+// 관리자비번 모달
+const pw_modal = document.querySelector('.pw_modal');
 const btn_modal = document.querySelector('.btn_modal');
-const btn_close = document.querySelector('.btn_close');
 
+// 공지삭제버튼 모달
+const remove_modal = document.querySelector('.remove_modal');
+const btn_remove = document.querySelector('.btn_remove');
+
+// 닫기 버튼
+const pw_btn_close = document.querySelector('.pw_btn_close');
+
+const remove_btn_close = document.querySelector('.remove_btn_close');
+const remove_cancel_btn = document.querySelector('.remove_cancel_btn');
+
+
+// 리스너
 btn_modal.addEventListener('click', () => {
-    modal.classList.add('active');
-    btn_close.classList.add('active');
+    pw_modal.classList.add('active');
+    pw_btn_close.classList.add('active');
 });
 
-btn_close.addEventListener('click', () => {
-    modal.classList.remove('active');
-    btn_close.classList.remove('active');
+btn_remove.addEventListener('click', () => {
+    remove_modal.classList.add('active');
+    remove_btn_close.classList.add('active');
 });
 
-function checkPwd() {
-	console.log("checkPwd실행");
-    var pwd = document.getElementById("managePwd").value;
-    if (pwd != 1111){
-      alert("관리자 패스워드가 일치하지 않습니다");
-      return false;
-    }
-    return true;
-}
+pw_btn_close.addEventListener('click', () => {
+    pw_modal.classList.remove('active');
+    pw_btn_close.classList.remove('active');
+});
+
+remove_btn_close.addEventListener('click', () => {
+    remove_modal.classList.remove('active');
+    remove_btn_close.classList.remove('active');
+});
+remove_cancel_btn.addEventListener('click', () => {
+    remove_modal.classList.remove('active');
+    remove_btn_close.classList.remove('active');
+});
