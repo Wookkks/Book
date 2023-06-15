b5b33afa4929793f737ba94a3228db81b059c9d9
Wookkks <discdao@gmail.com>
2023-06-15 09:32:44 +0900

    how 책 이미지 경로 수정 전 "img/책3.jpg" 후 "/img/책3.jpg"

--- bin/main/static/js/main.js
+++ bin/main/static/js/main.js
@@ -1,24 +1,40 @@
-// modal
-const modal = document.querySelector('.modal');
+// 관리자비번 모달
+const pw_modal = document.querySelector('.pw_modal');
 const btn_modal = document.querySelector('.btn_modal');
-const btn_close = document.querySelector('.btn_close');
 
+// 공지삭제버튼 모달
+const remove_modal = document.querySelector('.remove_modal');
+const btn_remove = document.querySelectorAll('.btn_remove');
+
+// 닫기 버튼
+const pw_btn_close = document.querySelector('.pw_btn_close');
+
+const remove_btn_close = document.querySelector('.remove_btn_close');
+const remove_cancel_btn = document.querySelector('.remove_cancel_btn');
+
+
+// 리스너
 btn_modal.addEventListener('click', () => {
-    modal.classList.add('active');
-    btn_close.classList.add('active');
+    pw_modal.classList.add('active');
+    pw_btn_close.classList.add('active');
 });
 
-btn_close.addEventListener('click', () => {
-    modal.classList.remove('active');
-    btn_close.classList.remove('active');
+pw_btn_close.addEventListener('click', () => {
+    pw_modal.classList.remove('active');
+    pw_btn_close.classList.remove('active');
 });
 
-function checkPwd() {
-	console.log("checkPwd실행");
-    var pwd = document.getElementById("managePwd").value;
-    if (pwd != 1111){
-      alert("관리자 패스워드가 일치하지 않습니다");
-      return false;
-    }
-    return true;
-}
+btn_remove.forEach(btn_remove => {
+    btn_remove.addEventListener('click', () => {
+        remove_modal.classList.add('active');
+        remove_btn_close.classList.add('active');
+    })
+});
+
+remove_close_arr = [remove_btn_close, remove_cancel_btn];
+remove_close_arr.forEach(close => {
+    close.addEventListener('click', () => {
+        remove_modal.classList.remove('active');
+        remove_btn_close.classList.remove('active');
+    })
+});
