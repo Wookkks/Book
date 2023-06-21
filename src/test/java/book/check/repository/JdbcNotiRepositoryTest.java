703b750f8fa43cdc727fc13ce6ae16fe7f95e74c
tbehippie00@gmail.com <tbehippie00@gmail.com>
2023-06-21 17:44:46 +0900

    JdbcNotiRepositoryTest 테스트코드 작성입니다.(저장/조회/수정)

--- src/test/java/book/check/repository/JdbcNotiRepositoryTest.java
+++ src/test/java/book/check/repository/JdbcNotiRepositoryTest.java
@@ -2,6 +2,8 @@ package book.check.repository;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,36 +12,37 @@ import book.check.domain.Noti;
 
 @SpringBootTest
 class JdbcNotiRepositoryTest {
-	
-	@Autowired JdbcNotiRepository repository;
 
+	@Autowired JdbcNotiRepository repository;
+	
 	@Test
 	void 공지저장() {
 		Noti noti = new Noti();
-		noti.setN_no(1111L);
-		noti.setN_title("title");
-		noti.setN_content("content");
+		noti.setN_no(1L);
+		noti.setN_title("제목");
+		noti.setN_content("내용ㅋ");
 		noti.setN_category("나눔책첵");
 		
 		Noti saveNoti = repository.saveNoti(noti);
 		assertThat(noti.getN_no()).isEqualTo(saveNoti.getN_no());
+	}
 	
-//	//findByNo
-//	@Test
-//	
-//	//findByTitle
-//	@Test
-//	
-//	//findAll
-//	@Test
-//	
-//	//updateNoti
-//	@Test
-//	
-//	//deleteNoti
-//	@Test
+	@Test
+	void 공지전체조회() {
+		 List<Noti> before = repository.findAll();
+		 
+		 List<Noti> after = repository.findAll();
+		 assertThat(after.size()).isEqualTo(before.size());
+	}
 	
-
+	@Test
+	void 공지수정() {
+		Noti noti = new Noti("제목1", "내용1", "나눔책첵");
+		Noti saveNoti = repository.saveNoti(noti); //아래에서 수정테스트를 위해 수정 전 데이터 저장
+		
+		Noti updateNoti = new Noti("수정후", "내용2", "분석책첵");
+		Noti result = repository.updateNoti(noti.getN_no(), updateNoti);
+		
+		assertThat(repository.findByNo(noti.getN_no()).get().getN_title()).isEqualTo(updateNoti.getN_title());
+	}
 }
-}
-
