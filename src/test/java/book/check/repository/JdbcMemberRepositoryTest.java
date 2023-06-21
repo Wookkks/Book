a8479c3e621ef2c8a7387325c00f205574fb3a26
tbehippie00@gmail.com <tbehippie00@gmail.com>
2023-06-21 18:05:35 +0900

    JdbcMemberRepository 멤버가입 test코드 작성입니다.

--- src/test/java/book/check/repository/JdbcMemberRepositoryTest.java
+++ src/test/java/book/check/repository/JdbcMemberRepositoryTest.java
@@ -1,14 +1,31 @@
-/*
- * package book.check.repository;
- * 
- * import static org.junit.jupiter.api.Assertions.fail;
- * 
- * import org.junit.jupiter.api.Test; import
- * org.springframework.boot.test.context.SpringBootTest;
- * 
- * @SpringBootTest class JdbcMemberRepositoryTest {
- * 
- * @Test void test() { fail("Not yet implemented"); }
- * 
- * }
- */
+package book.check.repository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.sql.Date;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import book.check.domain.Member;
+
+@SpringBootTest
+class JdbcMemberRepositoryTest {
+
+	@Autowired
+	MemberRepository repository;
+
+	@Test
+	void 멤버가입() {
+		Member member = new Member();
+		member.setM_name("길동");
+		member.setM_phone("01011112222");
+		member.setM_birth(new Date(2023, 06, 23));
+
+		Member saveMember = repository.saveMember(member);
+		assertThat(member.getM_no()).isEqualTo(saveMember.getM_no());
+
+	}
+
+}
